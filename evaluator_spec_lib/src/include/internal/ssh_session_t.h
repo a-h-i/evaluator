dba49fdc0e7d6ea68e90c0a86348c60e47543d4b
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2019-04-08 14:37:21 +0200

    ssh session connect

--- evaluator_spec_lib/src/include/internal/ssh_session_t.h
+++ evaluator_spec_lib/src/include/internal/ssh_session_t.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "dll_imports.h"
 #include "libssh/libssh.h"
+#include <string>
+#include <memory>
 
 namespace evspec {
 namespace EVSPEC_LOCAL ssh {
@@ -8,8 +10,21 @@ class ssh_session_t {
   ssh_session session_ {nullptr};
 
  public:
+  ssh_session_t(const std::string &host, const std::string &user, const std::string &key_path);
+  inline ssh_session_t(ssh_session session) : session_(session) {}
+  inline ssh_session_t(ssh_session_t &&other) {
+    std::swap(session_, other.session_);
+  }
+  ssh_session_t(const ssh_session_t &);
+  ssh_session_t operator=(const ssh_session_t &) = delete;
+  inline ssh_session_t& operator=(ssh_session_t &&other) {
+    std::swap(session_, other.session_);
+    return *this;
+  }
+  bool connect(std::string *what);
   ~ssh_session_t();
-  inline ssh_session operator->() { return session_; }
+  inline ssh_session operator->() const { return session_; }
+  inline operator ssh_session() const {return session_;}
 };
 }  // namespace EVSPEC_LOCALssh
 }  // namespace evspec
--- evaluator_spec_lib/src/ssh/session_t.cpp
+++ evaluator_spec_lib/src/ssh/session_t.cpp
@@ -1,10 +1,45 @@
+#include <stdexcept>
 #include "internal/ssh_session_t.h"
+
 namespace evspec {
 namespace ssh {
+
+static ssh_session allocate_session() {
+  ssh_session session = ssh_new();
+  if (session == nullptr) {
+    throw std::runtime_error(
+        "ssh::ssh_session_t could not allocate session object");
+  }
+  return session;
+}
 ssh_session_t::~ssh_session_t() {
   if (session_ != nullptr) {
     ssh_disconnect(session_);
     ssh_free(session_);
+  }
+}
+
+ssh_session_t::ssh_session_t(const std::string &host, const std::string &user,
+                             const std::string &key_path)
+    : session_(allocate_session()) {
+  ssh_options_set(session_, SSH_OPTIONS_HOST, host.c_str());
+  ssh_options_set(session_, SSH_OPTIONS_USER, user.c_str());
+  ssh_options_set(session_, SSH_OPTIONS_ADD_IDENTITY, key_path.c_str());
+}
+
+ssh_session_t::ssh_session_t(const ssh_session_t &other) {
+  ssh_options_copy(other, &session_);
+}
+
+bool ssh_session_t::connect(std::string *what) {
+  int status = ssh_connect(session_);
+  if(status == SSH_OK) {
+    return true;
+  } else {
+    if(what != nullptr) {
+      *what = ssh_get_error(session_);
+    }
+    return false;
   }
 }
 }  // namespace ssh
